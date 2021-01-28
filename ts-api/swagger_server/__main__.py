6af73c70c1462105a2390f93c0b057bca2a574a6
xukai <xvkai2010@gmail.com>
2021-01-28 23:56:49 +0900

    add cors, display chart

--- ts-api/swagger_server/__main__.py
+++ ts-api/swagger_server/__main__.py
@@ -3,10 +3,11 @@
 import connexion
 
 from swagger_server import encoder
-
+from flask_cors import CORS
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
+    CORS(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'ts api'}, pythonic_params=True)
     app.run(port=8080)
