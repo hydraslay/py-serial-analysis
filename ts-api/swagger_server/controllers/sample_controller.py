eb30d440806a3ccd297dbc6a71de225b06d4a865
xukai <xvkai2010@gmail.com>
2021-02-13 16:39:10 +0900

    impl api

--- ts-api/swagger_server/controllers/sample_controller.py
+++ ts-api/swagger_server/controllers/sample_controller.py
@@ -1,9 +1,28 @@
 import connexion
+import psycopg2
+
 import six
 
 from swagger_server.models.samples import Samples  # noqa: E501
 from swagger_server.models.samples_response import SamplesResponse  # noqa: E501
 from swagger_server import util
+
+
+def connect():
+    con = psycopg2.connect("host=" + "localhost" +
+                           " port=" + "5434" +
+                           " dbname=" + "ts" +
+                           " user=" + "test" +
+                           " password=" + "test")
+    return con
+
+
+def select_execute(con, sql):
+    with con.cursor() as cur:
+        cur.execute(sql)
+        rows = cur.fetchall()
+
+    return rows
 
 
 def get_samples():  # noqa: E501
@@ -14,7 +33,20 @@ def get_samples():  # noqa: E501
 
     :rtype: SamplesResponse
     """
-    return 'do some magic!'
+    conn = connect()
+    sql = """
+            SELECT uid, sample_data, value
+            FROM public.samples
+        """
+    rows = select_execute(conn, sql)
+    return {
+        'data': [{
+            "uid": row[0],
+            "sample_data": row[1],
+            "value": row[2]
+        } for row in rows],
+        'query_string': sql
+    }
 
 
 def set_sample_and_value_data(body):  # noqa: E501
@@ -29,4 +61,15 @@ def set_sample_and_value_data(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = [Samples.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
-    return 'do some magic!'
+
+    conn = connect()
+
+    sql = """INSERT INTO samples ("uid", "sample_data", "value") 
+        VALUES ({uid}, {sample_data}, {value}) 
+        ON CONFLICT ("uid") DO UPDATE SET 
+        sample_data={sample_data}, value={value}""".format_map({
+    })
+
+    conn.execute(sql)
+
+    return 'done'
